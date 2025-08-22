92bf5e383e25e9833290a4fcdbe51b16c600ae74
Nrlsb <luksbenitez01@hotmail.com>
2025-08-22 06:22:49 -0300

    Actualizacion V4.6.4

--- Frontend/mi-tienda-pintura/src/main.jsx
+++ Frontend/mi-tienda-pintura/src/main.jsx
@@ -1,14 +1,17 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // 1. Importar BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // 1. Importar HelmetProvider
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Envolver la aplicación con el Router */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* 2. Envolver la aplicación con el Provider */}
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 )
